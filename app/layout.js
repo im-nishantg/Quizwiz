c1b163569d3c27c255b2b660b2d0a58135adb325
im-nishantg <96784121+im-nishantg@users.noreply.github.com>
2023-07-10 03:07:03 +0530

    more bugs fixed

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,9 @@
 "use client"
 import Head from "next/head"
-import 'bootstrap/dist/css/bootstrap.css';
 import { SessionProvider } from "next-auth/react"
 import styles from "../styles/globals.css"
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 
 export default function RootLayout({ children }) {
